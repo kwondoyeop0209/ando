070457145d216a66508b7039373f76f561a5520d
Doyeop Kwon <kwon32220@gmail.com>
2021-09-27 17:28:41 +0900

    :sparkles: swagger 추가

--- backend/src/main/java/com/ssafy/SsafyApplication.java
+++ backend/src/main/java/com/ssafy/SsafyApplication.java
@@ -2,6 +2,9 @@ package com.ssafy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import java.nio.charset.Charset;
 
 @SpringBootApplication
 public class SsafyApplication {
@@ -9,5 +12,4 @@ public class SsafyApplication {
     public static void main(String[] args) {
         SpringApplication.run(SsafyApplication.class, args);
     }
-
 }
---
+++ backend/src/main/java/com/ssafy/config/SwaggerConfig.java
@@ -0,0 +1,25 @@
+package com.ssafy.config;
+
+import org.springframework.context.annotation.Bean;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * API 문서 관련 swagger2 설정 정의.
+ */
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket api() {
+
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.any())
+                .paths(PathSelectors.ant("/**"))
+                .build();
+    }
+}
