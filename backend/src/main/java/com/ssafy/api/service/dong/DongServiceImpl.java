e910cfc693be01e1e8b411fe3590bcc0c572ecc0
multicampus <jejin009@gmail.com>
2021-10-03 00:12:09 +0900

    :sparkles: feat: safety point, safety detail

--- backend/src/main/java/com/ssafy/api/service/dong/DongServiceImpl.java
+++ backend/src/main/java/com/ssafy/api/service/dong/DongServiceImpl.java
@@ -1,10 +1,17 @@
 package com.ssafy.api.service.dong;
 
+import com.ssafy.api.response.dong.SafetyRankingGetRes;
+import com.ssafy.api.response.dong.SpaceRankingGetRes;
 import com.ssafy.db.dto.dong.GetDongListDto;
 import com.ssafy.db.entity.Dong;
+import com.ssafy.db.mapping.DongRanking;
+import com.ssafy.db.mapping.DongSafetyIndex;
 import com.ssafy.db.repository.dongRepository;
+import com.ssafy.db.repository.dongRepositorySupport;
+import java.util.Optional;
 import javassist.NotFoundException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
@@ -16,6 +23,10 @@ import java.util.List;
 @RequiredArgsConstructor
 public class DongServiceImpl implements DongService{
     final dongRepository dongRepository;
+
+    @Autowired
+    dongRepositorySupport dongRepositorySupport;
+
     @Override
     public List<GetDongListDto> getDongList(Long id) throws NotFoundException {
         List<Dong> dongList = dongRepository.findBySiGunGu_Id(id);
@@ -27,4 +38,53 @@ public class DongServiceImpl implements DongService{
         Collections.sort(getDongListDtoList);
         return getDongListDtoList;
     }
+
+    @Override
+    public SafetyRankingGetRes getSafetyRanking(Long id) throws NotFoundException {
+        SafetyRankingGetRes safetyRankingGetRes = new SafetyRankingGetRes();
+
+        Optional<Dong> dongSafetyIndex = dongRepository.findById(id);
+
+        if(dongSafetyIndex.isPresent()) {
+            safetyRankingGetRes.setSafetyIndex(dongSafetyIndex.get().getSafetyIndex());
+            safetyRankingGetRes.setRankingOfSeoul(dongRepositorySupport.getRanking(id));
+            safetyRankingGetRes.setRankingOfGu(dongRepositorySupport.getGuRanking(id));
+        }else{
+           throw new NotFoundException("Dong id Empty");
+        }
+        return safetyRankingGetRes;
+    }
+
+    @Override
+    public SpaceRankingGetRes getSpaceRanking(Long id) throws NotFoundException {
+        SpaceRankingGetRes spaceRankingGetRes = new SpaceRankingGetRes();
+        System.out.println(1);
+
+        Optional<Dong> dong = dongRepository.findById(id);
+        System.out.println(2);
+
+        if(dong.isPresent()) {
+            spaceRankingGetRes.setCctvRanking(dongRepositorySupport.getSpaceRanking("cctv",id));
+            spaceRankingGetRes.setPoliceRanking(dongRepositorySupport.getSpaceRanking("police",id));
+            spaceRankingGetRes.setBarRanking(dongRepositorySupport.getSpaceRanking("bar",id));
+            spaceRankingGetRes.setLightRanking(dongRepositorySupport.getSpaceRanking("light",id));
+        System.out.println(3);
+
+            spaceRankingGetRes.setCctvAvg(dongRepositorySupport.getSpaceAvg("cctv"));
+            spaceRankingGetRes.setPoliceAvg(dongRepositorySupport.getSpaceAvg("police"));
+            spaceRankingGetRes.setBarAvg(dongRepositorySupport.getSpaceAvg("bar"));
+            spaceRankingGetRes.setLightAvg(dongRepositorySupport.getSpaceAvg("light"));
+        System.out.println(4);
+
+            spaceRankingGetRes.setCctv(dong.get().getCctvCnt());
+            spaceRankingGetRes.setBar(dong.get().getBarCnt());
+            spaceRankingGetRes.setPolice(dong.get().getPoliceCnt());
+            spaceRankingGetRes.setLight(dong.get().getLightCnt());
+        System.out.println(5);
+        }else{
+            throw new NotFoundException("Dong id Empty");
+        }
+        return spaceRankingGetRes;
+    }
+
 }
