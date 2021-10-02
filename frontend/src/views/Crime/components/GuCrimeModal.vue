7a6ea21a812a764d7d96984b6640813190831ad8
Yanghyesun <nbn0809@naver.com>
2021-10-03 01:45:39 +0900

    :sparkles: feat : 구선택시 년도 초기화 작업

--- frontend/src/views/Crime/components/GuCrimeModal.vue
+++ frontend/src/views/Crime/components/GuCrimeModal.vue
@@ -412,7 +412,14 @@ export default {
     };
   },
   watch: {
-    year: (val) => {
+    gu: function (val) {
+      //구가 바뀌면 다시 통신
+      console.log(val);
+
+      //년도 초기화해야함 => 2020년으로
+      this.$emit("initYear");
+    },
+    year: function (val) {
       //년도가 바뀌면 다시 통신
       console.log(val);
     },
@@ -434,7 +441,7 @@ export default {
       }
       this.isExpand = !this.isExpand;
     },
-    onCrimeDetail(){
+    onCrimeDetail() {
       // 00에 전달하는 값
     },
   },
